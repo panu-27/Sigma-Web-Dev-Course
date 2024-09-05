b3cf81f254d80ff30bcbb3fc2495a689ac7985d2
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 01:19:42 +0530

    array and functions on array

---
+++ Video 63/index.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5,7]
+// console.log(arr);
+// console.log(arr.length)
+// console.log(arr[0])
+// arr[0]=11;
+// console.log(arr[0])
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+// //push pop -ll
+// // shift  - que
+// //unshift - que fornt insert 
+// //concate method concate the multiple elements 
+// // sort to sort array
+// // slice method 
+// // arr = [1,empty] arr size = 2  , arr[1]= not allocated 
+
+
---
+++ Video 63/loops.js
@@ -0,0 +1,4 @@
+let a = [1,93,5,6,98]
+a.forEach((value ,index , arr) => {
+    console.log(value , index , arr)
+});
