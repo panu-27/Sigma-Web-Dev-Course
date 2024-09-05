8ee58f6c2dddd9c7eab8f91cf2ecf5d560f0acc2
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 00:35:07 +0530

    exercise - faulty calculator , math.random

---
+++ Video 59/index.js
@@ -0,0 +1,23 @@
+/*
+Create a faulty calculator using javascript 
+
+this faulty calculator does following :
+1. it takes two numbers as input from the user 
+2. it performs wrong operations as follow :
+
++ --->-
+* --->+
+- --->/
+/ --->**
+
+it performs wrong operation 10% of times 
+
+hint :
+use random fuction to generate raddom number form 1 to 100
+let a = Math.random()
+if (a<0.1){
+    faulty calcultor exicutes 
+}
+else{
+    normal calcultor 
+}*/
