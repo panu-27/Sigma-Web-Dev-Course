aae2c3708d90c0c1b41431f665db2f4646588e90
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 00:01:02 +0530

    operators , logical , assignment  , comparision , ternary operator

---
+++ Video 56/script.js
@@ -0,0 +1,23 @@
+console.log("hello i am conditional tutorial")
+
+let age = 1 ;
+let grace = 2;
+if((age+grace)>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+//operators +,*,-,/,**(exponential operator),% ,++ ,--
+//assignment operators += ,-= ,*= ,/= ,**=,%=
+//comaparision operators == ,=== ,>= ,<= ,!= ,!==,<,> , ?(ternary operator)
+//logical operators and (&&) , or(||) , logic not (!)
+
+// if else 
+// if else ladder
+
+
+//ternary operator 
+let c = 0;
+c = age>grace ?console.log(age):console.log(grace)
