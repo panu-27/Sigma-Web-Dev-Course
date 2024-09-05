641286bab3e40a3eef7c1206c437eb404b7e25d3
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 01:06:29 +0530

    random bussiness name generator without using array

--- Video 61/index.js
+++ Video 61/index.js
@@ -2,4 +2,6 @@ let a = 10;
 let b = 20;
 let c = '+';
 
-alert(`the result is ${eval(`${a} ${c} ${b}`)}`)
+console.log(`the result is ${eval(`${a} ${c} ${b}`)}`)
+
+// c=obj[c]; key c value is got form obj objects key c 
---
+++ Video 62/index.js
@@ -0,0 +1,18 @@
+/*create a busssiness name generatoe by combining list of adjactives and shop name and another word 
+
+Adjactives :
+Crazy
+Amazing
+Fire
+
+Shop Name :
+Engine 
+Foods 
+Garments
+
+Another Word:
+Bors
+Limites
+Hub
+
+*/
