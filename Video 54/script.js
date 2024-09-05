a98e02d2eb38ed45a96b2e6ed2389e2755efa043
panu-27 <pranavzinjad06@gmail.com>
2024-09-05 23:18:44 +0530

    javascript start

---
+++ Video 54/script.js
@@ -0,0 +1,12 @@
+console.log("print in the console")
+alert("hi you  got a warning")
+var isTrue = confirm("Do you confirm that you want to execute this true or false")
+if (isTrue) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+//manipulate css 
+document.body.style.backgroundColor = "red";
+console.log("just casual")
