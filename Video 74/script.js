765c5ce540d5c2fbdaafafc6b6902cc8a1f2a261
panu-27 <pranavzinjad06@gmail.com>
2024-09-07 00:31:32 +0530

    evenlisner , setTimer , setInterval

---
+++ Video 74/script.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("dblclick" , ()=>{
+    document.querySelector(".box").innerHTML="enjoy you click"
+})
+button.addEventListener("contextmenu" , ()=>{
+ alert(" dont") //on right click
+})
+// visit mdn eventlisner mouse events , keyboard events 
+//eventbubbling?
+//e.stoppropgation
+// setInterval - a function which set a event that occur after some inetval to time reguraly 
+
+// setTimer - once after timeer ends 
+
+
+
