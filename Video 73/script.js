6b6fd9956be5f28ebaad550b404e56fe6d3aa506
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 23:55:22 +0530

    exercise add thumbnil to

---
+++ Video 73/script.js
@@ -0,0 +1,5 @@
+function createCard(title , channel_name , views , months_old , duration , thumbnil ) {
+    //finish the function 
+}
+
+createCard("Sigma WEb dev intro to blc" ,"codewith harry " , 56000000 , 2 , "31.23" ,"url of thumbnil")
