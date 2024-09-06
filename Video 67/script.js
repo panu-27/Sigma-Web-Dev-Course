78b96daea0518a289d9b46e6a5afab1448b04095
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 22:26:38 +0530

    dom and bom

---
+++ Video 67/script.js
@@ -0,0 +1,11 @@
+//docuement.body.childnode -- in browser
+// cont.lastElementChild.style.backgroi= "red"
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
+// document.body.firstElementChild.childNodes
+// NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// document.body.firstElementChild.children[3].nextElementSibling
+// document.body.firstElementChild.children[3].previousElementSibling
+// document.body.firstElementChild.children[3].parentElement
+// document.body.childern[1]
+// 
