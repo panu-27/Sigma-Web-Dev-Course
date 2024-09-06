1421d1d56585b39006b4c02fc6c88f6ecfdcb840
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 22:52:05 +0530

    query selector

---
+++ Video 68/index.js
@@ -0,0 +1,15 @@
+console.log("hey");
+
+// let jogies = document.getElementsByClassName("box");
+// console.log(jogies);
+// jogies[1].style.backgorundColor = "red";
+
+// document.getElementById("b").style.backgroundColor = "red";
+document.querySelector(".box").style.backgroundColor = "green";
+//impotant
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// })
+
+
