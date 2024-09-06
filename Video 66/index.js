159e486f634cf0da2bd42dc5cc9663d4f2defab4
panu-27 <pranavzinjad06@gmail.com>
2024-09-06 22:03:10 +0530

    DOM Starts

---
+++ Video 66/index.js
@@ -0,0 +1,5 @@
+document.title = "HI ITS OUR NEW TITLE";//html document titile 
+console.log(document.title)
+console.log(document.body)
+document.body.style.backgroundColor = "green";
+// $0 the element you select becomes dollar zero
