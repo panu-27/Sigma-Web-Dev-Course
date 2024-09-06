c6e88f76f8b4965c6235e534ec8c3a581f404037
panu-27 <pranavzinjad06@gmail.com>
2024-09-07 01:15:45 +0530

    async await fetch api post get

---
+++ Video 76/index.js
@@ -0,0 +1,9 @@
+//async and await function 
+async function name(params) {
+    
+}
+// async function continues to run in background 
+await // it stops to execute until the function executes 
+//it can be used in async code block
+//fetch api 
+// post and fetch request
